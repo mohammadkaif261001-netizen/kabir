a9670cbecbdfff27d16910819ebe9fd0cb15295b
Kaif <mohammadkaif261001@gmail.com>
2026-01-19 22:39:44 +0600

    added kabir.cpp

---
+++ kabir.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, count = 0;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    cout << "Total even numbers = " << count;
+
+    return 0;
+}
